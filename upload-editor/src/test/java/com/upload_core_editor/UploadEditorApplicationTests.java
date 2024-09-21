65d1cfa5a1a0c6cb3e420fc8fbcf0d10b08f7212
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2024-09-21 00:26:27 -0300

    Adiciona estrutura do projeto

---
+++ upload-editor/src/test/java/com/upload_core_editor/UploadEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upload_core_editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
